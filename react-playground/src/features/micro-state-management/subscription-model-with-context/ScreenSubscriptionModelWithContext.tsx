3cb5212f0f19ce71a81563ed3e588fbf643bc6c1
jaychang99 <jaychang99@gmail.com>
2024-03-09 21:40:18 +0900

    feat: 다중 컨텍스트 중첩 시 예시 추가

--- react-playground/src/features/micro-state-management/subscription-model-with-context/ScreenSubscriptionModelWithContext.tsx
+++ react-playground/src/features/micro-state-management/subscription-model-with-context/ScreenSubscriptionModelWithContext.tsx
@@ -3,6 +3,7 @@ import {
   ThemeComponent,
   ThemeContext,
 } from '@/features/micro-state-management/subscription-model-with-context/components/ThemeComponent';
+import { StoreProvider } from '@/features/micro-state-management/subscription-model-with-context/contexts/storeContext';
 
 export const ScreenSubscriptionModelWithContext = () => {
   return (
@@ -14,7 +15,19 @@ export const ScreenSubscriptionModelWithContext = () => {
         </ThemeContext.Provider>
       </ThemeContext.Provider>
       <h2>컨텍스트를 이용한 구독 모델</h2>
+      <h3>Provider 없이 기본 Store 사용</h3>
       <CounterComponent />
+      <CounterComponent />
+      <StoreProvider initialState={{ count: 10 }}>
+        <h3>Provider를 이용한 Store 사용</h3>
+        <CounterComponent />
+        <CounterComponent />
+        <StoreProvider initialState={{ count: 20 }}>
+          <h3>Provider 중첩 사용</h3>
+          <CounterComponent />
+          <CounterComponent />
+        </StoreProvider>
+      </StoreProvider>
     </div>
   );
 };
