67b69249d21262bcc7c9f32dc2979ad587420fd6
jaychang99 <jaychang99@gmail.com>
2024-03-09 09:08:05 +0900

    feat: 구독 모듈 초기 필요한 부분 세팅

--- react-playground/src/features/micro-state-management/subscription-model/ScreenSubscriptionModel.tsx
+++ react-playground/src/features/micro-state-management/subscription-model/ScreenSubscriptionModel.tsx
@@ -1,3 +1,17 @@
+/* eslint-disable react-refresh/only-export-components */
+let state = {
+  count: 0,
+};
+
+type State = typeof state;
+type NextState = State | ((state: State) => State);
+
+export const getState = () => state;
+export const setState = (nextState: NextState) => {
+  // state = nextState; -> 함수로 state update 를 시도할 수 있으므로... 아래 코드 사용
+  state = typeof nextState === 'function' ? nextState(state) : nextState;
+};
+
 const ScreenSubscriptionModel = () => {
   return <div>ScreenSubscriptionModel</div>;
 };
