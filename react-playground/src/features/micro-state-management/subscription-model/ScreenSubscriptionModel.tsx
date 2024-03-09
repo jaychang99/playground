c88ea5e09a97fef6ed6c56269692e80ccecb4d35
jaychang99 <jaychang99@gmail.com>
2024-03-09 10:06:24 +0900

    feat: useStore 함수 구현

--- react-playground/src/features/micro-state-management/subscription-model/ScreenSubscriptionModel.tsx
+++ react-playground/src/features/micro-state-management/subscription-model/ScreenSubscriptionModel.tsx
@@ -3,6 +3,7 @@ import {
   Component1,
   Component2,
 } from '@/features/micro-state-management/subscription-model/components/Component1';
+import { Dispatch, SetStateAction, useEffect, useState } from 'react';
 
 export const createContainer = <T,>(initialState: T) => {
   let state = initialState;
@@ -21,8 +22,8 @@ export type CreateStore = typeof createStore;
 type UpdateFunction<T> = (prevState: T) => T;
 type NextState<T> = T | UpdateFunction<T>;
 type Action<T> = T | UpdateFunction<T>;
-type Unsubscribe = () => void;
 type Callback = () => void;
+type Unsubscribe = () => void;
 type CreateStoreReturnType<T> = {
   getState: () => T;
   setState: (action: Action<T>) => void;
@@ -48,6 +49,23 @@ export const createStore = <T,>(initialState: T): CreateStoreReturnType<T> => {
   };
 
   return { getState, setState, subscribe };
+};
+
+type UseStoreReturnType<T> = [T, Dispatch<SetStateAction<T>>];
+export const useStore = <T,>(
+  store: CreateStoreReturnType<T>,
+): UseStoreReturnType<T> => {
+  const [state, setState] = useState(store.getState());
+
+  useEffect(() => {
+    const unsubscribe = store.subscribe(() => {
+      setState(store.getState());
+    });
+    setState(store.getState()); // useEffect 가 뒤늦게 실행되어서 store 가 이미 새로운 상태를 가지고 있을 가능성이 있기 때문.
+    return unsubscribe;
+  }, [store]);
+
+  return [state, store.setState] as const;
 };
 
 const ScreenSubscriptionModel = () => {
