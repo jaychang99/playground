8f6fcc197675412320c9238b55039a3083809212
jaychang99 <jaychang99@gmail.com>
2024-03-09 21:09:57 +0900

    feat: 전역 상태에 context 복습

--- react-playground/src/features/micro-state-management/subscription-model-with-context/ScreenSubscriptionModelWithContext.tsx
+++ react-playground/src/features/micro-state-management/subscription-model-with-context/ScreenSubscriptionModelWithContext.tsx
@@ -1,3 +1,17 @@
+import {
+  ThemeComponent,
+  ThemeContext,
+} from '@/features/micro-state-management/subscription-model-with-context/components/ThemeComponent';
+
 export const ScreenSubscriptionModelWithContext = () => {
-  return <div>ScreenSubscriptionModelWithContext</div>;
+  return (
+    <div>
+      <h2>컨텍스트 복습</h2>
+      <ThemeContext.Provider value="dark">
+        <ThemeContext.Provider value="light">
+          <ThemeComponent />
+        </ThemeContext.Provider>
+      </ThemeContext.Provider>
+    </div>
+  );
 };
