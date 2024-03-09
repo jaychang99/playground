9f3a122406456df0f7de9fcfbeba6c65437c06c7
jaychang99 <jaychang99@gmail.com>
2024-03-09 09:19:43 +0900

    feat: 컨테이너를 생성하는 함수를 만들어 세팅, 아직 렌더링 트리거는 되지 않음

--- react-playground/src/features/micro-state-management/subscription-model/ScreenSubscriptionModel.tsx
+++ react-playground/src/features/micro-state-management/subscription-model/ScreenSubscriptionModel.tsx
@@ -1,19 +1,33 @@
 /* eslint-disable react-refresh/only-export-components */
-let state = {
-  count: 0,
-};
 
-type State = typeof state;
-type NextState = State | ((state: State) => State);
+type UpdateFunction<T> = (state: T) => T;
+type NextState<T> = T | UpdateFunction<T>;
 
-export const getState = () => state;
-export const setState = (nextState: NextState) => {
-  // state = nextState; -> 함수로 state update 를 시도할 수 있으므로... 아래 코드 사용
-  state = typeof nextState === 'function' ? nextState(state) : nextState;
+export const createContainer = <T,>(initialState: T) => {
+  let state = initialState;
+  const getState = () => state;
+  const setState = (nextState: NextState<T>) => {
+    state =
+      typeof nextState === 'function'
+        ? (nextState as UpdateFunction<T>)(state) // 실제로 함수 자체가 state 인지, update function 이 들어온 것인지 typescript 는 알 수가 없음.
+        : nextState;
+  };
+
+  return { getState, setState };
 };
 
 const ScreenSubscriptionModel = () => {
-  return <div>ScreenSubscriptionModel</div>;
+  const { getState, setState } = createContainer({ count: 0 });
+
+  const handleClick = () => {
+    setState((state) => ({ ...state, count: state.count + 1 }));
+  };
+
+  return (
+    <div>
+      <button onClick={handleClick}>current count: {getState().count}</button>
+    </div>
+  );
 };
 
 export default ScreenSubscriptionModel;
