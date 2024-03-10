1cd93990f3a1710446dc092a0bada4ebbdf8b896
jaychang99 <jaychang99@gmail.com>
2024-03-10 10:58:54 +0900

    feat: 리렌더링 문제가 되는 케이스 상정

--- react-playground/src/features/micro-state-management/global-state-management-libraries/components/RerenderingOptimization.tsx
+++ react-playground/src/features/micro-state-management/global-state-management-libraries/components/RerenderingOptimization.tsx
@@ -1,3 +1,23 @@
+// eslint-disable-next-line prefer-const
+let state = {
+  a: 1,
+  b: { c: 2, d: 3 },
+  e: { f: 4, g: 5 },
+};
+
+const ComponentA = () => {
+  return <div>value: {state.b.c}</div>;
+};
+
+const ComponentB = () => {
+  return <div>value: {state.e.g}</div>;
+};
+
 export const RerenderingOptimization = () => {
-  return <div>RerenderingOptimization</div>;
+  return (
+    <div>
+      <ComponentA />
+      <ComponentB />
+    </div>
+  );
 };
