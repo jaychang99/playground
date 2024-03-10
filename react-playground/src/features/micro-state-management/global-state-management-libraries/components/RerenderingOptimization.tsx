e4b397bf8170d8b7e9c408b63f256d45f92ac37f
jaychang99 <jaychang99@gmail.com>
2024-03-10 11:04:58 +0900

    feat: 내가 구현해본 가상의 셀렉터 함수

--- react-playground/src/features/micro-state-management/global-state-management-libraries/components/RerenderingOptimization.tsx
+++ react-playground/src/features/micro-state-management/global-state-management-libraries/components/RerenderingOptimization.tsx
@@ -1,3 +1,5 @@
+import { Selector } from '@/features/micro-state-management/subscription-model/modules/useStore';
+
 // eslint-disable-next-line prefer-const
 let state = {
   a: 1,
@@ -5,12 +7,25 @@ let state = {
   e: { f: 4, g: 5 },
 };
 
+type State = typeof state;
+
+/**
+ * 컴포넌트 사용처에서 selector 함수를 넘겨 줘야 한다는 점에서 수동 렌더링 최적화이다.
+ */
+const useSelector = <S,>(selector: Selector<State, S>) => {
+  return selector(state);
+};
+
 const ComponentA = () => {
-  return <div>value: {state.b.c}</div>;
+  const value = useSelector((state) => state.b.c);
+
+  return <div>value: {value}</div>;
 };
 
 const ComponentB = () => {
-  return <div>value: {state.e.g}</div>;
+  const value = useSelector((state) => state.e.g);
+
+  return <div>value: {value}</div>;
 };
 
 export const RerenderingOptimization = () => {
