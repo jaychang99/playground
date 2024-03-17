49bd378f69f3c29b614ad9e46a2f2cd641e7982e
jaychang99 <jaychang99@gmail.com>
2024-03-17 10:15:01 +0900

    fix(Zustand): selector 함수를 이용하여 리렌더링 최적화

--- react-playground/src/features/micro-state-management/example-zustand/ScreenExampleZustand.tsx
+++ react-playground/src/features/micro-state-management/example-zustand/ScreenExampleZustand.tsx
@@ -1,12 +1,13 @@
 import { useStore } from '@/features/micro-state-management/example-zustand/stores/store';
 
 function ScreenExampleZustand() {
-  const { count } = useStore();
+  // const { count } = useStore();
+  const count = useStore((state) => state.count); // 수동 렌더링 최적화
 
   console.log('__COMPONENT_RERENDERED__'); // rerender 확인용 로그
 
   const handleClickUpdateText = () => {
-    useStore.setState({ text: 'world' }); // 화면과 무관한 text 를 업데이트했지만, 화면이 리렌더링된다.
+    useStore.setState({ text: 'world' }); // selector 함수를 useStore 에 넘겼으므로, 이제 text 가 변경되더라도 컴포넌트가 불필요하게 리렌더링되지 않는다.
   };
 
   return (
