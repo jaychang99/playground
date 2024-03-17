dc118a974cc455e6c023b2f8f834816e7d70ebec
jaychang99 <jaychang99@gmail.com>
2024-03-17 10:17:45 +0900

    feat(Banner): selector 함수를 잘못 구성하는 케이스 추가

--- react-playground/src/features/micro-state-management/example-zustand/ScreenExampleZustand.tsx
+++ react-playground/src/features/micro-state-management/example-zustand/ScreenExampleZustand.tsx
@@ -2,7 +2,8 @@ import { useStore } from '@/features/micro-state-management/example-zustand/stor
 
 function ScreenExampleZustand() {
   // const { count } = useStore();
-  const count = useStore((state) => state.count); // 수동 렌더링 최적화
+  // const count = useStore((state) => state.count); // 수동 렌더링 최적화
+  const [{ count }] = useStore((state) => [{ count: state.count }]); // 수동 렌더링 최적화 - 잘못된 사용 케이스 -> 선택자 함수가 새 객체를 포함해 새로운 배열을 생성하므로 렌더링 최적화가 불가능하다.
 
   console.log('__COMPONENT_RERENDERED__'); // rerender 확인용 로그
 
