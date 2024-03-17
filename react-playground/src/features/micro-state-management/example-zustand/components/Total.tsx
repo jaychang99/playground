e07fa58e11135d983f92943fdec9817d752561e7
jaychang99 <jaychang99@gmail.com>
2024-03-17 10:50:25 +0900

    feat(Zustand): Total 에서 count1 과 count2 의 함계 자체를 selector 함수로 써서 엣지 케이스도 대비

--- react-playground/src/features/micro-state-management/example-zustand/components/Total.tsx
+++ react-playground/src/features/micro-state-management/example-zustand/components/Total.tsx
@@ -1,20 +1,22 @@
 import { useCounterStore } from '@/features/micro-state-management/example-zustand/stores/useCounterStore';
 import { Store } from '@/features/micro-state-management/example-zustand/types/StoreState';
 
-const selectCount1 = (state: Store) => state.count1;
-const selectCount2 = (state: Store) => state.count2;
+// const selectCount1 = (state: Store) => state.count1;
+// const selectCount2 = (state: Store) => state.count2;
+const selectTotal = (state: Store) => state.count1 + state.count2;
 
 export const Total = () => {
-  const count1 = useCounterStore(selectCount1);
-  const count2 = useCounterStore(selectCount2);
+  // const count1 = useCounterStore(selectCount1);
+  // const count2 = useCounterStore(selectCount2);
+  const total = useCounterStore(selectTotal);
 
   console.log('<Total /> has been rerendered');
 
   return (
     <div>
       <h2>Total</h2>
-      {/* 문제 - 만약 count1 이 줄어든 만큼, count2 가 늘어나서 총합이 변경되지 않더라도, Total 컴포넌트는 여전히 리렌더링 될 것이다.  */}
-      <p>Total: {count1 + count2}</p>
+      {/* total 이 변경될 때에만 리렌더링된다.  */}
+      <p>Total: {total}</p>
     </div>
   );
 };
