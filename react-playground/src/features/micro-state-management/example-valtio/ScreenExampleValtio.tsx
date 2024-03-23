c879257ae106ff4ac6b30bebeba467b8ab285616
jaychang99 <jaychang99@gmail.com>
2024-03-23 21:49:14 +0900

    feat(Valtio): proxy 와 useSnapshot 을 이용해서 React 에 실제 적용해보기

--- react-playground/src/features/micro-state-management/example-valtio/ScreenExampleValtio.tsx
+++ react-playground/src/features/micro-state-management/example-valtio/ScreenExampleValtio.tsx
@@ -1,11 +1,44 @@
-import { proxyObject } from '@/features/micro-state-management/example-valtio/constants/proxyObject';
-import './modules/proxySnapshotTest';
+// import './modules/proxySnapshotTest'; -> 실행하려면 주석 해제
+import { proxy, useSnapshot } from 'valtio';
+
+const state = proxy({
+  count1: 0,
+  count2: 0,
+});
+
+const Counter1 = () => {
+  const snap = useSnapshot(state);
+  const increment = () => ++state.count1;
+
+  console.log('Counter 1 has been re-rendered');
+
+  return (
+    <div>
+      {snap.count1} <button onClick={increment}>+1</button>
+    </div>
+  );
+};
+
+const Counter2 = () => {
+  const snap = useSnapshot(state);
+  const increment = () => ++state.count2;
+
+  console.log('Counter 2 has been re-rendered');
+
+  return (
+    <div>
+      {snap.count2} <button onClick={increment}>+1</button>
+    </div>
+  );
+};
 
 function ScreenExampleValtio() {
   return (
     <div>
       <h2>Example Valtio</h2>
-      <button onClick={() => proxyObject.count++}>Increment</button>
+      {/* <button onClick={() => proxyObject.count++}>Increment</button> */}
+      <Counter1 />
+      <Counter2 />
     </div>
   );
 }
