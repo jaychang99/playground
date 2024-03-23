735acd072c72c183d7f325b3f627abb231f75f9a
jaychang99 <jaychang99@gmail.com>
2024-03-23 21:32:47 +0900

    refactor(Valtio): proxyObject 를 별도 파일로 분리

--- react-playground/src/features/micro-state-management/example-valtio/ScreenExampleValtio.tsx
+++ react-playground/src/features/micro-state-management/example-valtio/ScreenExampleValtio.tsx
@@ -1,28 +1,4 @@
-const initialState = {
-  count: 0,
-  text: 'hello',
-};
-type InitialState = typeof initialState;
-
-/**
- * 어떤 객체의 어떤 프로퍼티를 변경하려고 하는지 감지할 수 있다.
- */
-const proxyObject = new Proxy<InitialState>(initialState, {
-  set: (target, prop, value) => {
-    console.log('start setting', prop, value);
-
-    if (prop === 'count') {
-      target[prop] = value as number;
-    }
-
-    if (prop === 'text') {
-      target[prop] = value as string;
-    }
-
-    console.log('end setting', prop, value);
-    return true;
-  },
-});
+import { proxyObject } from '@/features/micro-state-management/example-valtio/constants/proxyObject';
 
 function ScreenExampleValtio() {
   return (
