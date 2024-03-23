56ab04c12320f033d09a779b81acd789edacca52
jaychang99 <jaychang99@gmail.com>
2024-03-23 21:40:27 +0900

    refactor(Valtio): snapshot 부분 테스트한 영역 분리

--- react-playground/src/features/micro-state-management/example-valtio/ScreenExampleValtio.tsx
+++ react-playground/src/features/micro-state-management/example-valtio/ScreenExampleValtio.tsx
@@ -1,25 +1,5 @@
 import { proxyObject } from '@/features/micro-state-management/example-valtio/constants/proxyObject';
-import { proxy, snapshot } from 'valtio';
-
-const state = proxy({ count: 0 });
-const snap1 = snapshot(state);
-
-/**
- * state, snap1은 같은 값을 가지고 있지만, 서로 다른 객체이다.(서로 다른 참조를 가진다. )
- */
-console.log('state: ', state); // { count: 0 }, proxy 로 감싼 변경 가능한 객체
-console.log('snap1: ', snap1); // { count: 0 }, Object.freeze() 로 동결되어 변경 불가능한 객체
-console.log('state === snap1: ', state === snap1); // false
-
-++state.count;
-
-const snap2 = snapshot(state);
-
-console.log('state: ', state); // { count: 1 }
-console.log('snap2: ', snap2); // { count: 1 }
-console.log('snap1: ', snap1); // { count: 0 }
-console.log('state === snap2: ', state === snap2); // false
-console.log('snap1 === snap2: ', snap1 === snap2); // false
+import './modules/proxySnapshotTest';
 
 function ScreenExampleValtio() {
   return (
