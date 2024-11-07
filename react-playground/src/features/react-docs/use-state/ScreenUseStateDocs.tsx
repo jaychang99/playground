78db6c1a0755723f169e4c185b1dab7ca1ee9584
jaychang99 <jaychang99@gmail.com>
2024-11-07 22:03:48 +0900

    feat: 6번 문제

--- react-playground/src/features/react-docs/use-state/ScreenUseStateDocs.tsx
+++ react-playground/src/features/react-docs/use-state/ScreenUseStateDocs.tsx
@@ -1,44 +1,14 @@
 import { useState } from 'react';
 
-const DATA1 = {
-  name: 'John',
-  age: 25,
-};
-
-const DATA2 = {
-  name: 'John',
-  age: 25,
-};
-
 const App = () => {
-  const [data, setData] = useState(DATA1);
-  const [count, setCount] = useState(0);
-
-  const handleUpdateData = () => {
-    setData(DATA2);
-  };
-
-  const handleUpdateCount = () => {
-    setCount(0);
-  };
+  const [foo, setFoo] = useState(console.log('hello, world'));
 
   return (
     <div>
-      <Children />
-      <p>{count} 회 클릭하였습니다. </p>
-      <button onClick={handleUpdateCount}>카운트 업데이트 버튼</button>
-      <button onClick={handleUpdateData}>데이터 업데이트 버튼</button>
-      <div>
-        <p>{data.name}</p>
-        <p>{data.age}</p>
-      </div>
+      <p>{foo}</p>
+      <button onClick={() => setFoo(1)}>Increment</button>
     </div>
   );
 };
 
 export default App;
-
-const Children = () => {
-  console.log('Children render');
-  return <div>Children</div>;
-};
