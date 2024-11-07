23a5a5a68ecd3dbc69faa9f6b1c156181401edae
jaychang99 <jaychang99@gmail.com>
2024-11-07 21:12:09 +0900

    feat: 2 번 문제

--- react-playground/src/features/react-docs/use-state/ScreenUseStateDocs.tsx
+++ react-playground/src/features/react-docs/use-state/ScreenUseStateDocs.tsx
@@ -1,18 +1,16 @@
 import { useState } from 'react';
 
-const updateState = (prev: number) => {
-  console.log(prev);
-
-  return prev + 1;
-};
-
 const App = () => {
   const [count, setCount] = useState(0);
+
+  const handleClick = () => {
+    setCount((prev) => prev + 1);
+  };
 
   return (
     <div>
       <p>{count} 회 클릭하였습니다. </p>
-      <button onClick={() => setCount(updateState)}>1 증가 시키기</button>
+      <button onClick={() => handleClick()}>1 증가 시키기</button>
     </div>
   );
 };
