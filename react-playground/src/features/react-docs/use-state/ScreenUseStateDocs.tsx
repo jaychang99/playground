d3675b06de1a2b2be64e6a1ff5bd8a6062e43b88
jaychang99 <jaychang99@gmail.com>
2024-11-07 22:22:57 +0900

    feat: 7번 문제

--- react-playground/src/features/react-docs/use-state/ScreenUseStateDocs.tsx
+++ react-playground/src/features/react-docs/use-state/ScreenUseStateDocs.tsx
@@ -1,12 +1,25 @@
-import { useState } from 'react';
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useEffect, useState } from 'react';
 
 const App = () => {
-  const [foo, setFoo] = useState(console.log('hello, world'));
+  const [count, setCount] = useState(1);
+
+  const logHelloWorld = () => {
+    console.log('hello, world');
+  };
+
+  useEffect(() => {
+    console.log('hello, world');
+  }, [setCount]);
+
+  useEffect(() => {
+    logHelloWorld();
+  }, [logHelloWorld]);
 
   return (
     <div>
-      <p>{foo}</p>
-      <button onClick={() => setFoo(1)}>Increment</button>
+      <p>{count}</p>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
     </div>
   );
 };
