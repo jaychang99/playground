b3a7357b8da99c354d169e8ae8c45a2b6d807de2
jaychang99 <jaychang99@gmail.com>
2024-11-07 21:53:16 +0900

    feat: 5번 문제 (가장 복잡)

--- react-playground/src/features/react-docs/use-state/ScreenUseStateDocs.tsx
+++ react-playground/src/features/react-docs/use-state/ScreenUseStateDocs.tsx
@@ -1,22 +1,44 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import { useState } from 'react';
 
+const DATA1 = {
+  name: 'John',
+  age: 25,
+};
+
+const DATA2 = {
+  name: 'John',
+  age: 25,
+};
+
 const App = () => {
-  const [minute, setMinute] = useState(60);
-  const [count, setCount] = useState(minute);
+  const [data, setData] = useState(DATA1);
+  const [count, setCount] = useState(0);
 
-  const handleClick = () => {
-    setMinute((prev) => prev - 1);
+  const handleUpdateData = () => {
+    setData(DATA2);
+  };
 
-    console.log(count);
+  const handleUpdateCount = () => {
+    setCount(0);
   };
 
   return (
     <div>
+      <Children />
       <p>{count} 회 클릭하였습니다. </p>
-      <button onClick={handleClick}>버튼</button>
+      <button onClick={handleUpdateCount}>카운트 업데이트 버튼</button>
+      <button onClick={handleUpdateData}>데이터 업데이트 버튼</button>
+      <div>
+        <p>{data.name}</p>
+        <p>{data.age}</p>
+      </div>
     </div>
   );
 };
 
 export default App;
+
+const Children = () => {
+  console.log('Children render');
+  return <div>Children</div>;
+};
