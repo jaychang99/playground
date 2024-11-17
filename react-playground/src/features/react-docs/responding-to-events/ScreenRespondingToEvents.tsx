ff7e90efe70f02cc045f3357b34f2cbd6548c498
jaychang99 <jaychang99@gmail.com>
2024-11-17 15:33:46 +0900

    feat: 3번 문제

--- react-playground/src/features/react-docs/responding-to-events/ScreenRespondingToEvents.tsx
+++ react-playground/src/features/react-docs/responding-to-events/ScreenRespondingToEvents.tsx
@@ -1,36 +1,17 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-
-import { useState } from 'react';
+const logHelloWorld = () => console.log('Hello, world');
 
 const App = () => {
-  const [counter, setCounter] = useState(0);
-
-  const handleClick = () => {
-    setCounter(counter + 1);
-  };
-
-  return <Button onClick={handleClick}>{counter}</Button>;
-};
-
-const Button = ({ onClick, ...props }) => {
   return (
-    <div>
-      <p>This is a Button</p>
-      <button
-        {...props}
-        style={{
-          backgroundColor: 'blue',
-          color: 'white',
-          padding: '10px 20px',
-          border: 'none',
-          borderRadius: '5px',
-          cursor: 'pointer',
-          fontSize: '16px',
-          fontWeight: 'bold',
-        }}
-      />
+    <div onClick={logHelloWorld}>
+      <Button>
+        <Button onClick={logHelloWorld}>Click Me!</Button>
+      </Button>
     </div>
   );
+};
+
+const Button = ({ children }) => {
+  return <div onClick={logHelloWorld}>{children}</div>;
 };
 
 export default App;
