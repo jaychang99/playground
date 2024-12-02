099d741f356ddbe3b458f904014f4d0dcd42084f
jaychang99 <jaychang99@gmail.com>
2024-12-02 21:23:08 +0900

    feat: 1번

--- react-playground/src/features/react-docs/state-as-a-snapshot/ScreenStateAsASnapshot.tsx
+++ react-playground/src/features/react-docs/state-as-a-snapshot/ScreenStateAsASnapshot.tsx
@@ -1,5 +1,26 @@
+import { useState } from 'react';
+
 const App = () => {
-  return <div>ScreenStateAsASnapshot</div>;
+  const [count, setCount] = useState(0);
+
+  const handleClick = () => {
+    setTimeout(() => {
+      setCount((prev) => prev + 1);
+    }, 1000);
+
+    setTimeout(() => {
+      setCount(count + 1);
+    }, 2000);
+
+    setCount(count + 1);
+  };
+
+  return (
+    <div>
+      <button onClick={handleClick}>Click me</button>
+      <div>Count: {count}</div>
+    </div>
+  );
 };
 
 export default App;
