0ad4b664acbb853ff3b611831741019842e012b7
jaychang99 <jaychang99@gmail.com>
2024-12-02 21:48:12 +0900

    feat: 2번

--- react-playground/src/features/react-docs/render-and-commit/ScreenRenderAndCommit.tsx
+++ react-playground/src/features/react-docs/render-and-commit/ScreenRenderAndCommit.tsx
@@ -1,5 +1,29 @@
+import { useState } from 'react';
+
 const App = () => {
-  return <div>ScreenRenderAndCommit</div>;
+  const [count, setCount] = useState(0);
+  const [fruit, setFruit] = useState('apple');
+
+  const handleClick = () => {
+    setCount((prev) => prev + 1);
+
+    if (count % 3 === 0) {
+      setFruit('banana');
+    } else {
+      setFruit('apple');
+    }
+  };
+
+  return (
+    <>
+      <button onClick={handleClick}>Click me</button>
+      <div>
+        <h1 id="title">Hello, world</h1>
+        <p id="count">Count: {count}</p>
+        <p id="fruit">Fruit: {fruit}</p>
+      </div>
+    </>
+  );
 };
 
 export default App;
