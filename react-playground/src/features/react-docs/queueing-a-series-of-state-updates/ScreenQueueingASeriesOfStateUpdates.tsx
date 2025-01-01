91ada7aadc036f636bb5575d3cb0c487714a424a
jaychang99 <jaychang99@gmail.com>
2025-01-01 13:21:40 +0900

    feat: 2번 문제

--- react-playground/src/features/react-docs/queueing-a-series-of-state-updates/ScreenQueueingASeriesOfStateUpdates.tsx
+++ react-playground/src/features/react-docs/queueing-a-series-of-state-updates/ScreenQueueingASeriesOfStateUpdates.tsx
@@ -1,24 +1,35 @@
 import { useState } from 'react';
 
 const App = () => {
-  const [isButtonDisabled, setIsButtonDisabled] = useState(false);
+  const [pendingItemsCount, setPendingItemsCount] = useState(0);
+  const [completedItemsCount, setCompletedItemsCount] = useState(0);
 
-  const handleClickLikePost = async () => {
-    setIsButtonDisabled(true);
+  async function handleClick() {
+    setPendingItemsCount(pendingItemsCount + 1);
 
-    setTimeout(() => {
-      console.log('Post liked!');
-      setIsButtonDisabled(false);
-    }, 3000);
-  };
+    await delay(3000);
+
+    setPendingItemsCount((pendingItemsCount) => pendingItemsCount - 1);
+
+    setCompletedItemsCount(completedItemsCount + 1);
+  }
 
   return (
     <div>
-      <button disabled={isButtonDisabled} onClick={handleClickLikePost}>
-        Like Post
-      </button>
+      <div>대기 중: {pendingItemsCount} 건</div>
+      <div>완료: {completedItemsCount} 건</div>
+
+      <button onClick={handleClick}>주문 넣기</button>
     </div>
   );
 };
+
+// ms 만큼의 시간이 지연된 Promise를 반환하는 함수
+// 즉, 이 함수를 await 할 시 ms 만큼 로직 실행 지연
+function delay(ms: number) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+}
 
 export default App;
