d54dfce7adfe5b81426051593a07a4414e259ec2
jaychang99 <jaychang99@gmail.com>
2025-01-01 16:39:25 +0900

    2번 문제

--- react-playground/src/features/react-docs/updating-objects-in-state/ScreenUpdatingObjectsInState.tsx
+++ react-playground/src/features/react-docs/updating-objects-in-state/ScreenUpdatingObjectsInState.tsx
@@ -2,26 +2,44 @@
 import { useState } from 'react';
 
 const App = () => {
-  const [isButtonClicked, setIsButtonClicked] = useState(false);
-
   const [user, setUser] = useState({
-    name: 'Mark',
-    age: 39,
+    user: {
+      name: 'John',
+      age: 25,
+    },
+    book: {
+      title: 'The Great Gatsby',
+      publishedIn: 1925,
+      author: {
+        name: 'F. Scott Fitzgerald',
+        bornIn: 1896,
+      },
+    },
   });
 
-  const handleClickIncreaseAge = () => {
-    user.age = user.age + 1;
-    setIsButtonClicked(true);
+  const handleButtonClick = () => {
+    setUser({
+      ...user,
+      user: {
+        ...user.user,
+        age: 26,
+      },
+      book: {
+        ...user.book,
+        publishedIn: 1926,
+      },
+    });
   };
+
+  console.log('User Updated');
 
   return (
     <div>
-      <div>
-        <div>Name: {user.name}</div>
-        <div>Age: {user.age}</div>
-      </div>
-
-      <button onClick={handleClickIncreaseAge}>Age + 1</button>
+      <button onClick={handleButtonClick}>Update User</button>
+      <p>{`User name: ${user.user.name}`}</p>
+      <p>{`User age: ${user.user.age}`}</p>
+      <p>{`Book title: ${user.book.title}`}</p>
+      <p>{`Book published in: ${user.book.publishedIn}`}</p>
     </div>
   );
 };
