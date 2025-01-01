4395005cb27df12474cdf67e0f6cad47085ec16c
jaychang99 <jaychang99@gmail.com>
2025-01-01 13:38:51 +0900

    feat: 1번 문제

--- react-playground/src/features/react-docs/updating-objects-in-state/ScreenUpdatingObjectsInState.tsx
+++ react-playground/src/features/react-docs/updating-objects-in-state/ScreenUpdatingObjectsInState.tsx
@@ -1,5 +1,29 @@
-const ScreenUpdatingObjectsInState = () => {
-  return <div>ScreenUpdatingObjectsInState</div>;
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { useState } from 'react';
+
+const App = () => {
+  const [isButtonClicked, setIsButtonClicked] = useState(false);
+
+  const [user, setUser] = useState({
+    name: 'Mark',
+    age: 39,
+  });
+
+  const handleClickIncreaseAge = () => {
+    user.age = user.age + 1;
+    setIsButtonClicked(true);
+  };
+
+  return (
+    <div>
+      <div>
+        <div>Name: {user.name}</div>
+        <div>Age: {user.age}</div>
+      </div>
+
+      <button onClick={handleClickIncreaseAge}>Age + 1</button>
+    </div>
+  );
 };
 
-export default ScreenUpdatingObjectsInState;
+export default App;
